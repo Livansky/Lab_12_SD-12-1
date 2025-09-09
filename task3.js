1d01ad819fc871eed3611fba4c0299f380bf06d1
Livansky <carlos72ortiz14@gmail.com>
2025-09-09 11:21:45 -0600

    tercera task

--- task3.js
+++ task3.js
@@ -1,2 +1,12 @@
 // Task 3: addUser(first_name, last_name, email)
-
+export function addUser(first_name, last_name, email) {
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ first_name, last_name, email })
+    })
+    .then(res => res.json());
+}
+addUser("Carlos", "Ortiz", "carlos72ortiz14@gmail.com");
